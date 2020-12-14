c38181c59c21d2c164d044639bfbf12121e1f6ea
vitality <youtao271@gmail.com>
2020-12-14 17:38:48 +0800

    update

---
+++ pan/precache-manifest.4b915b5b4a32fa897bdfda38735e45a5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa403610a27ea4c6a6d055655a9d28b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6dff23d13b0f35cd982",
+    "url": "./static/css/2.7917c474.chunk.css"
+  },
+  {
+    "revision": "8a4608556fc478ee1729",
+    "url": "./static/css/main.3b257a41.chunk.css"
+  },
+  {
+    "revision": "f6dff23d13b0f35cd982",
+    "url": "./static/js/2.f514f058.chunk.js"
+  },
+  {
+    "revision": "045520932a3d853c3c91e0f044b4a102",
+    "url": "./static/js/2.f514f058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4608556fc478ee1729",
+    "url": "./static/js/main.31afacf5.chunk.js"
+  },
+  {
+    "revision": "471cffa62640e12447cd",
+    "url": "./static/js/runtime-main.e9cd6480.js"
+  },
+  {
+    "revision": "55f1a02dfde1f9254efae3c1dc1ae691",
+    "url": "./static/media/audio.55f1a02d.svg"
+  },
+  {
+    "revision": "003232bd4a76cb78b7a489004b0351cb",
+    "url": "./static/media/icomoon.003232bd.ttf"
+  },
+  {
+    "revision": "2e76ac3b800b59ebb6373337d3885513",
+    "url": "./static/media/icomoon.2e76ac3b.svg"
+  },
+  {
+    "revision": "4e1aaf7a0e1a84cdb3c459c16bc8369b",
+    "url": "./static/media/icomoon.4e1aaf7a.woff"
+  },
+  {
+    "revision": "fe30e84925ad3d2e6869f842e85f189f",
+    "url": "./static/media/icomoon.fe30e849.eot"
+  },
+  {
+    "revision": "7c84423bd2a8a761bdeec9ac55fba42c",
+    "url": "./static/media/lg-bg.7c84423b.svg"
+  },
+  {
+    "revision": "694100c9ffcb84337f8dd13305cd4c35",
+    "url": "./static/media/lg-fg.694100c9.svg"
+  },
+  {
+    "revision": "2eafb726dd15d97a63edefb5ee9ab0a0",
+    "url": "./static/media/zip.2eafb726.svg"
+  }
+]);
